555160110336cfd0b5b15f90e46fe5503945a949
cohyunoh <coh00@stuy.edu>
2019-03-02 23:20:21 -0500

    tested more mazes and it works

--- driver.java
+++ driver.java
@@ -3,15 +3,17 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class driver{
   public static void main(String[] args) {
-    try{
-      Maze maze = new Maze("Maze1.txt");
-      System.out.println(maze);
-      System.out.println(maze.setupCorrect());
-      maze.setAnimate(true);
-      System.out.println(maze.solve());
-    }catch(FileNotFoundException e){
-      e.printStackTrace();
-    }
+    String filename = "data" + args[0] + ".dat";
+      try{
+        Maze f;
+        f = new Maze(filename);//true animates the maze.
+
+        f.setAnimate(true);
+        System.out.println(f.solve());
+        System.out.println(f);
+      }catch(FileNotFoundException e){
+        System.out.println("Invalid filename: "+filename);
+      }
 
   }
 }
